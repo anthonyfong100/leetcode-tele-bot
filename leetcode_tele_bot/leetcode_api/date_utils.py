ebda98d25763fef60cf782999e7cf58a4e3c96af
Anthony <anthonyfong100@gmail.com>
2021-05-29 20:23:46 +0800

    add in code for leetcode api

---
+++ leetcode_tele_bot/leetcode_api/date_utils.py
@@ -0,0 +1,48 @@
+import datetime
+import math
+from calendar import monthrange
+from typing import Tuple
+
+import leetcode_tele_bot.leetcode_api.constants as constants
+from leetcode_tele_bot.leetcode_api.models import Month
+
+
+def get_today_date() -> Tuple[int]:
+    """
+    Return year month date in the format (YYYY,MM,DD) in UTC time
+    """
+    dt = datetime.datetime.now(datetime.timezone.utc)
+    return (dt.year, dt.month, dt.day)
+
+
+def get_week_in_month(dt: datetime.datetime) -> int:
+    # leetcode website counts week as weeks from starting date of the month
+    # e.g. if month starts at friday --> next monday is still week 1 (week doesnt end on sunday, is strictly 7 days)
+    dom = dt.day
+    return int(math.ceil(dom / 7.0))
+
+
+def get_week_offset_from_start(curr_date: datetime.datetime) -> int:
+    # return the dt object for the monday on the start date (start date is a fixed time calculated from Jan 1st 2021)
+    years_offset = (
+        curr_date.year - constants.START_DATETIME.year
+    ) * constants.NUM_WEEKS_PER_YEAR
+    month_offset = constants.WEEK_OFFSET_FROM_START_MONTH_MAP[
+        Month(curr_date.month)
+    ]
+    week_offset = get_week_in_month(curr_date) - 1
+    return years_offset + month_offset + week_offset
+
+
+def get_problem_offset_from_start(curr_date: datetime.datetime) -> int:
+    # leetcode has 7 daily problem and one weekly problem a week
+
+    days_offset = (curr_date - constants.START_DATETIME).days
+    problem_offset = days_offset + get_week_offset_from_start(
+        curr_date
+    )  # additional weekly problem
+    return problem_offset + 1  # first problem of week is weekly problem
+
+
+def get_num_days_in_month(year: int, month: int) -> int:
+    return monthrange(year, month)[1]
---
+++ tests/leetcode_api/date_utils_test.py
@@ -0,0 +1,51 @@
+import datetime
+
+from leetcode_tele_bot.leetcode_api.date_utils import (
+    get_problem_offset_from_start,
+    get_week_in_month,
+    get_week_offset_from_start,
+)
+
+
+def test_get_week_in_month_normal_case():
+    may_1_2021 = datetime.datetime(2021, 5, 1, tzinfo=datetime.timezone.utc)
+    assert get_week_in_month(may_1_2021) == 1
+
+    may_7_2021 = datetime.datetime(2021, 5, 7, tzinfo=datetime.timezone.utc)
+    assert get_week_in_month(may_7_2021) == 1
+
+
+def test_get_week_in_month_boundary_val():
+    may_28_2021 = datetime.datetime(2021, 5, 28, tzinfo=datetime.timezone.utc)
+    assert get_week_in_month(may_28_2021) == 4
+
+    may_29_2021 = datetime.datetime(2021, 5, 29, tzinfo=datetime.timezone.utc)
+    assert get_week_in_month(may_29_2021) == 5
+
+    jan_7_2021 = datetime.datetime(2021, 1, 7, tzinfo=datetime.timezone.utc)
+    assert get_week_in_month(jan_7_2021) == 1
+
+
+def test_get_week_offset_from_start():
+    jan_1_2021 = datetime.datetime(2021, 1, 1, tzinfo=datetime.timezone.utc)
+    assert get_week_offset_from_start(jan_1_2021) == 0
+
+    jan_7_2021 = datetime.datetime(2021, 1, 7, tzinfo=datetime.timezone.utc)
+    assert get_week_offset_from_start(jan_7_2021) == 0
+
+    feb_1_2021 = datetime.datetime(2021, 2, 1, tzinfo=datetime.timezone.utc)
+    assert get_week_offset_from_start(feb_1_2021) == 5
+
+    may_28_2021 = datetime.datetime(2021, 5, 28, tzinfo=datetime.timezone.utc)
+    assert get_week_offset_from_start(may_28_2021) == 22
+
+
+def test_get_problem_offset_from_start():
+    jan_1_2021 = datetime.datetime(2021, 1, 1, tzinfo=datetime.timezone.utc)
+    assert get_problem_offset_from_start(jan_1_2021) == 1
+
+    jan_7_2021 = datetime.datetime(2021, 1, 7, tzinfo=datetime.timezone.utc)
+    assert get_problem_offset_from_start(jan_7_2021) == 7
+
+    feb_1_2021 = datetime.datetime(2021, 2, 1, tzinfo=datetime.timezone.utc)
+    assert get_problem_offset_from_start(feb_1_2021) == 37

f3d7f16a922bae86fa80ae863e5794375f9d4aee
Anthony <anthonyfong100@gmail.com>
2021-05-31 20:33:23 +0800

    add digital ocean deploy command

---
+++ scripts/digital_ocean_deploy.sh
@@ -0,0 +1,9 @@
+doctl registry login --expiry-seconds 180
+docker pull registry.digitalocean.com/anthonyfong97/leetcode-tele-bot:latest
+
+echo "starting server instance..."
+docker run -d \
+  --restart always \
+  -p 0.0.0.0:80:80 \
+  --name leetcode-tele-bot \
+  registry.digitalocean.com/anthonyfong97/leetcode-tele-bot:latest

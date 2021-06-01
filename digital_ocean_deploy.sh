259a37c0c917ad2de7644966ae5cea7cfb80dd95
Anthony <anthonyfong100@gmail.com>
2021-06-01 20:06:04 +0800

    change deploy

--- digital_ocean_deploy.sh
+++ digital_ocean_deploy.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-cd ~
-wget https://github.com/digitalocean/doctl/releases/download/v1.60.0/doctl-1.60.0-linux-amd64.tar.gz
-tar xf ~/doctl-1.60.0-linux-amd64.tar.gz
-mv ~/doctl /usr/local/bin
 
-doctl auth init --access-token TOKEN_NAME
-doctl registry login --expiry-seconds 180 --access-token TOKEN_NAME
-docker pull registry.digitalocean.com/anthonyfong97/leetcode-tele-bot:latest
+echo "Logging in into docker"
+docker login -u DOCKER_USERNAME -p DOCKER_PASSWORD
+
+echo "Pulling image"
+docker pull anthonyfong/leetcode-tele-bot:latest
 
 echo "starting server instance..."
 docker run -d \
   --restart always \
   -p 0.0.0.0:80:80 \
   --name leetcode-tele-bot \
-  registry.digitalocean.com/anthonyfong97/leetcode-tele-bot:latest
+  anthonyfong/leetcode-tele-bot:latest
